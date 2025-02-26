3ac0919e6983a2d34fea47afb16c026b67b3288e
dhynarazarine <dinar.andiyani@gmail.com>
2025-02-26 15:54:44 +0700

    Minggu 1

---
+++ Minggu 1/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
+
+
+// mengimpor kelas ItemController dari namespace, kita bisa menggunakan ItemController dalam file routing Laravel
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('items', ItemController::class);
+// Route::resource metode yang secara otomatis berdasarkan metode yang ada di ItemController
