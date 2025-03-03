dc116e24843f4b23d8c5a415992b344d0dbeed48
dhynarazarine <dinar.andiyani@gmail.com>
2025-03-04 01:43:44 +0700

    Jobsheet 3

--- Minggu 3/PWL_POS/app/Http/Controllers/LevelController.php
+++ Minggu 3/PWL_POS/app/Http/Controllers/LevelController.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 class LevelController extends Controller
 {
     public function index(){
-    //     DB::insert('insert into m_level (level_kode, level_nama, created_at, updated_at) values (?, ?, ?, ?)', 
-    // ['CUS', 'Pelanggan', now(), now()]);
-    //     return 'Insert data baru berhasil';
+        DB::insert('insert into m_level (level_kode, level_nama, created_at, updated_at) values (?, ?, ?, ?)', 
+    ['CUS', 'Pelanggan', now(), now()]);
+        return 'Insert data baru berhasil';
 
     // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
     // return 'Update data berhasil. Jumlah data yang diupdate'. $row .' Baris';
@@ -18,8 +18,8 @@ class LevelController extends Controller
     // $row = DB::delete('delete from m_level where level_kode = ?',['CUS']);
     // return 'Delete data berhasil. Jumlah data yang dihapus '.$row.' Baris';
 
-    $data = DB::select('select * from m_level');
-    return view('level', ['data' => $data]);
+    // $data = DB::select('select * from m_level');
+    // return view('level', ['data' => $data]);
 
     }
 }
--- Minggu 3/PWL_POS/routes/web.php
+++ Minggu 3/PWL_POS/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,4 @@ Route::get('/', function(){
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
