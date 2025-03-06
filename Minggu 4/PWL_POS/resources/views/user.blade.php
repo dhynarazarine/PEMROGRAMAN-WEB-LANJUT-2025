3accbdc4ebcdf72fc1c242a1498af294bc6ff621
dhynarazarine <dinar.andiyani@gmail.com>
2025-03-06 11:30:58 +0700

    first commit

---
+++ Minggu 4/PWL_POS/resources/views/user.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2">
+        <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>IDPengguna</th>
+        </tr>
+
+        @foreach ($data as $d)
+            <tr>
+                <td>{{$d->user_id}}</td>
+                <td>{{$d->username}}</td>
+                <td>{{$d->nama}}</td>
+                <td>{{$d->level_id}}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
