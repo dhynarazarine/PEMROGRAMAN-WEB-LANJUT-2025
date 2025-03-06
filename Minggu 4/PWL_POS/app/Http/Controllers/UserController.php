4204b73205063185f0fa1b0f72248981b4ae8ec0
dhynarazarine <dinar.andiyani@gmail.com>
2025-03-06 20:48:34 +0700

    Praktikum 2.1 – Retrieving Single Models

--- Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu 4/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,18 +9,27 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index()
-    {
-        // tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+    {      
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create ($data);
+        // $user = UserModel::find(1); // Mengambil data pengguna dengan ID 1 dari database. mencari data berdasarkan primary key
+        // $user = UserModel::where('level_id', 1)->first(); // Mencari data berdasarkan kolom terentu
+        // $user = UserModel::firstWhere('level_id', 1); // Mencari satu baris pertama dalam DB dimana level id 1 tetapi lebih ringkas
 
-      
-        $user = UserModel::all();
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(484);
+        // });
+         // Kode digunakan untuk mencari data pengguna berdasarkan id = 1. Jika error maka memunculkan 484
+
+         $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(484);
+         });
+        // kode error karena tidak menemukan id = 20 didalam tabel DB
         return view('user', ['data' => $user]);
 
     }
